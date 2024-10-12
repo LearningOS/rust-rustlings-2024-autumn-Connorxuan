c1afbbfaf7eaf02e2f562c0397f16f63f36f14fd
connorxuan <connorxuan@outlook.com>
2024-10-13 00:16:57 +0800

    modified:   exercises/algorithm/algorithm10.rs 	modified:   exercises/enums/enums1.rs 	modified:   exercises/enums/enums2.rs 	modified:   exercises/enums/enums3.rs 	modified:   exercises/hashmaps/hashmaps1.rs 	modified:   exercises/hashmaps/hashmaps2.rs 	modified:   exercises/hashmaps/hashmaps3.rs 	modified:   exercises/modules/modules1.rs 	modified:   exercises/modules/modules2.rs 	modified:   exercises/modules/modules3.rs 	modified:   exercises/options/options1.rs 	modified:   exercises/options/options2.rs 	modified:   exercises/options/options3.rs 	modified:   exercises/quiz2.rs 	modified:   exercises/strings/strings1.rs 	modified:   exercises/strings/strings2.rs 	modified:   exercises/strings/strings3.rs 	modified:   exercises/strings/strings4.rs 	modified:   exercises/structs/structs1.rs 	modified:   exercises/structs/structs2.rs 	modified:   exercises/structs/structs3.rs

--- exercises/algorithm/algorithm10.rs
+++ exercises/algorithm/algorithm10.rs
@@ -2,7 +2,6 @@
 	graph
 	This problem requires you to implement a basic graph functio
 */
-// I AM NOT DONE
 
 use std::collections::{HashMap, HashSet};
 use std::fmt;
@@ -16,29 +15,13 @@ impl fmt::Display for NodeNotInGraph {
 pub struct UndirectedGraph {
     adjacency_table: HashMap<String, Vec<(String, i32)>>,
 }
-impl Graph for UndirectedGraph {
-    fn new() -> UndirectedGraph {
-        UndirectedGraph {
-            adjacency_table: HashMap::new(),
-        }
-    }
-    fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>> {
-        &mut self.adjacency_table
-    }
-    fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>> {
-        &self.adjacency_table
-    }
-    fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
-    }
-}
 pub trait Graph {
     fn new() -> Self;
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>>;
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
     fn add_node(&mut self, node: &str) -> bool {
         //TODO
-		true
+        true
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
         //TODO
@@ -59,6 +42,32 @@ pub trait Graph {
         edges
     }
 }
+impl Graph for UndirectedGraph {
+    fn new() -> UndirectedGraph {
+        UndirectedGraph {
+            adjacency_table: HashMap::new(),
+        }
+    }
+    fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>> {
+        &mut self.adjacency_table
+    }
+    fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>> {
+        &self.adjacency_table
+    }
+    fn add_edge(&mut self, edge: (&str, &str, i32)) {
+        //TODO
+        let (node1, node2, weight) = edge;
+        self.adjacency_table
+            .entry(node1.to_string())
+            .or_insert_with(Vec::new)
+            .push((node2.to_string(), weight));
+        self.adjacency_table
+            .entry(node2.to_string())
+            .or_insert_with(Vec::new)
+            .push((node1.to_string(), weight));
+    }
+}
+
 #[cfg(test)]
 mod test_undirected_graph {
     use super::Graph;
